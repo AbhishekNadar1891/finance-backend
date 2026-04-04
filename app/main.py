983c35ac812520bfc235e4d2f52b17efee29fa4d
Abhishek Nadar <nadarbalaabhishek@gmail.com>
2026-04-04 18:19:20 +0530

    Added user creation API with PostgreSQL integration

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.models.user import User
+from app.routes.users import router as user_router
 
 app = FastAPI()
 
-# create tables
 Base.metadata.create_all(bind=engine)
+
+app.include_router(user_router)
 
 @app.get("/")
 def root():
